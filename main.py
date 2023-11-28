6afa86e7b32888779e2c5a814f0b6ad4f3b76c67
Bosie <obosieakioyamen1@gmail.com>
2023-11-27 22:34:07 -0800

    Added appendix to what for the graph that gets outputted when eda file is called + commented out un implemented file calls in main

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # main.py
 
 # Import functions from other Python files
-from game_of_thrones_logistic_regression import logistic_regression_function
-from random_forest import random_forest_function
-from gradient_boosting import gradient_boosting_function
-from exploratory_data import exploratory_data_function
+#from game_of_thrones_logistic_regression import logistic_regression_function
+#from random_forest import random_forest_function
+#from gradient_boosting import gradient_boosting_function
+from exploratory_data import exploratory_data_analysis
 
 def main():
     # Call functions from other files
-    logistic_regression_function()
-    random_forest_function()
-    gradient_boosting_function()
-    exploratory_data_function()
+    # logistic_regression_function()
+    # random_forest_function()
+    # gradient_boosting_function()
+    exploratory_data_analysis()
 
 if __name__ == "__main__":
     main()
