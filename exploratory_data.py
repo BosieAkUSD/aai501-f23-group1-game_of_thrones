6f3e934e6d1049972c94053ce494fa9728182eb5
Bosie <obosieakioyamen1@gmail.com>
2023-11-28 20:25:03 -0800

    Storing EDA files in appendix since when ran it does not close the graphs and return to terminal. Adding steps for LR model, adding some functionality to LR model

--- exploratory_data.py
+++ exploratory_data.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -11,90 +8,96 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from collections import Counter
 import matplotlib.patches as mpatches
+import warnings
+
+# Use Non-Interactive Backend:
+# set the matplotlib backend to a non-interactive one:
+mpl.use('Agg')
+
+# Ignore seaborn categorical dtype warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="seaborn")
 
 def exploratory_data_analysis():
-  battles = pd.read_csv("data_files/battles.csv")
-  character_deaths = pd.read_csv("data_files/character-deaths.csv")
-  character_predictions = pd.read_csv("data_files/character-predictions.csv")
-  
-  battles.loc[:, "defender_count"] = (4 - battles[["defender_1", "defender_2", "defender_3", "defender_4"]].isnull().sum(axis = 1))
-  battles.loc[:, "attacker_count"] = (4 - battles[["attacker_1", "attacker_2", "attacker_3", "attacker_4"]].isnull().sum(axis = 1))
-  
-  
-  battles.info()
-  
-  battles.head()
-  
-  # Check for Missing Values
-  print("Missing Values:\n", battles.isnull().sum())
-  
-  # Handle missing values 
-  battles["attacker_king"].fillna("Unknown", inplace=True)
-  battles["defender_king"].fillna("Unknown", inplace=True)
-  battles["attacker_outcome"].fillna("Unknown", inplace=True)
-  
-  # Examine the Distribution of Battles Across Years
-  plt.figure(figsize=(10, 6))
-  sns.countplot(x="year", data=battles)
-  plt.title("Distribution of Battles Across Years")
-  plt.show()
-  
-  majorevents = battles.groupby('year').sum()[["major_death", "major_capture"]].plot.bar(rot = 0)
-  _ = majorevents.set(xlabel = "Year", ylabel = "No. of Death/Capture Events", ylim = (0, 9)), majorevents.legend(["Major Deaths", "Major Captures"])
-  
-  # Explore Types of Battles and Their Outcomes
-  plt.figure(figsize=(12, 6))
-  sns.countplot(x="battle_type", hue="attacker_outcome", data=battles)
-  plt.title("Battle Types and Outcomes")
-  plt.show()
-  
-  # Investigate the Involvement of Attackers and Defenders
-  attackers = battles[["attacker_1", "attacker_2", "attacker_3", "attacker_4"]].stack().value_counts()
-  defenders = battles[["defender_1", "defender_2", "defender_3", "defender_4"]].stack().value_counts()
-  
-  # Plotting the top attackers and defenders
-  plt.figure(figsize=(14, 6))
-  attackers.head(10).plot(kind="bar", color="orange", alpha=0.7, label="Attackers")
-  defenders.head(10).plot(kind="bar", color="blue", alpha=0.7, label="Defenders")
-  plt.title("Top Attackers and Defenders")
-  plt.legend()
-  plt.show()
-  
-  # Analyze the Size of Attacking and Defending Forces
-  plt.figure(figsize=(12, 6))
-  sns.scatterplot(x="attacker_size", y="defender_size", data=battles, hue="attacker_outcome")
-  plt.title("Size of Attacking and Defending Forces")
-  plt.show()
-  
-  character_deaths.info()
-  
-  character_deaths.head()
-  
-  # Check for Missing Values
-  print("Missing Values:\n", character_deaths[["Death Year", "Book of Death", "Death Chapter"]].isnull().sum())
-  
-  # Explore Gender Distribution
-  plt.figure(figsize=(8, 5))
-  sns.countplot(x="Gender", data=character_deaths)
-  plt.title("Gender Distribution of Characters")
-  plt.show()
-  
-  # Analyze Nobility Status
-  plt.figure(figsize=(8, 5))
-  sns.countplot(x="Nobility", data=character_deaths)
-  plt.title("Nobility Status of Characters")
-  plt.show()
-  
-  # Investigate Allegiances and Their Relation to Fates
-  plt.figure(figsize=(12, 6))
-  sns.countplot(x="Allegiances", hue="Death Year", data=character_deaths)
-  plt.xticks(rotation=45, ha="right")
-  plt.title("Allegiances and Death Years")
-  plt.show()
-  
-  # Examine Death-related Columns
-  plt.figure(figsize=(14, 6))
-  sns.countplot(x="Death Year", hue="Book of Death", data=character_deaths)
-  plt.title("Death Year and Book of Death")
-  plt.show()
+    battles = pd.read_csv("data_files/battles.csv")
+    character_deaths = pd.read_csv("data_files/character-deaths.csv")
+    character_predictions = pd.read_csv("data_files/character-predictions.csv")
 
+    battles.loc[:, "defender_count"] = (4 - battles[["defender_1", "defender_2", "defender_3", "defender_4"]].isnull().sum(axis=1))
+    battles.loc[:, "attacker_count"] = (4 - battles[["attacker_1", "attacker_2", "attacker_3", "attacker_4"]].isnull().sum(axis=1))
+
+    battles.info()
+
+    battles.head()
+
+    # Check for Missing Values
+    print("Missing Values:\n", battles.isnull().sum())
+
+    # Handle missing values
+    battles["attacker_king"].fillna("Unknown", inplace=True)
+    battles["defender_king"].fillna("Unknown", inplace=True)
+    battles["attacker_outcome"].fillna("Unknown", inplace=True)
+
+    # Examine the Distribution of Battles Across Years
+    plt.figure(figsize=(10, 6))
+    sns.countplot(x="year", data=battles)
+    plt.title("Distribution of Battles Across Years")
+    plt.savefig('appendix/distribution_of_battles.png')
+
+    majorevents = battles.groupby('year').sum()[["major_death", "major_capture"]].plot.bar(rot=0)
+    _ = majorevents.set(xlabel="Year", ylabel="No. of Death/Capture Events", ylim=(0, 9)), majorevents.legend(["Major Deaths", "Major Captures"])
+
+    # Explore Types of Battles and Their Outcomes
+    plt.figure(figsize=(12, 6))
+    sns.countplot(x="battle_type", hue="attacker_outcome", data=battles)
+    plt.title("Battle Types and Outcomes")
+    plt.savefig('appendix/battle_types_and_outcomes.png')
+
+    # Investigate the Involvement of Attackers and Defenders
+    attackers = battles[["attacker_1", "attacker_2", "attacker_3", "attacker_4"]].stack().value_counts()
+    defenders = battles[["defender_1", "defender_2", "defender_3", "defender_4"]].stack().value_counts()
+
+    # Plotting the top attackers and defenders
+    plt.figure(figsize=(14, 6))
+    attackers.head(10).plot(kind="bar", color="orange", alpha=0.7, label="Attackers")
+    defenders.head(10).plot(kind="bar", color="blue", alpha=0.7, label="Defenders")
+    plt.title("Top Attackers and Defenders")
+    plt.legend()
+    plt.savefig('appendix/top_attackers_and_defenders.png')
+
+    # Analyze the Size of Attacking and Defending Forces
+    plt.figure(figsize=(12, 6))
+    sns.scatterplot(x="attacker_size", y="defender_size", data=battles, hue="attacker_outcome")
+    plt.title("Size of Attacking and Defending Forces")
+    plt.savefig('appendix/size_forces.png')
+
+    character_deaths.info()
+
+    character_deaths.head()
+
+    # Check for Missing Values
+    print("Missing Values:\n", character_deaths[["Death Year", "Book of Death", "Death Chapter"]].isnull().sum())
+
+    # Explore Gender Distribution
+    plt.figure(figsize=(8, 5))
+    sns.countplot(x="Gender", data=character_deaths)
+    plt.title("Gender Distribution of Characters")
+    plt.savefig('appendix/gender_distribution.png')
+
+    # Analyze Nobility Status
+    plt.figure(figsize=(8, 5))
+    sns.countplot(x="Nobility", data=character_deaths)
+    plt.title("Nobility Status of Characters")
+    plt.savefig('appendix/nobility_status_characters.png')
+
+    # Investigate Allegiances and Their Relation to Fates
+    plt.figure(figsize=(12, 6))
+    sns.countplot(x="Allegiances", hue="Death Year", data=character_deaths)
+    plt.xticks(rotation=45, ha="right")
+    plt.title("Allegiances and Death Years")
+    plt.savefig('appendix/allegiances_and_death_years.png')
+
+    # Examine Death-related Columns
+    plt.figure(figsize=(14, 6))
+    sns.countplot(x="Death Year", hue="Book of Death", data=character_deaths)
+    plt.title("Death Year and Book of Death")
+    plt.savefig('appendix/death_year_and_book_of_death.png')
--- game_of_thrones_logistic_regression.py
+++ game_of_thrones_logistic_regression.py
@@ -1,47 +1,90 @@
-""" 
-1.  Load Datasets: 
-   - Use pandas to load the datasets (`battles.csv`, `character-deaths.csv`, `character-predictions.csv`).
+# Import necessary libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
-2.  Explore and Preprocess Data: 
-   - Use pandas to explore the structure of each dataset.
-   - Handle any missing values in the datasets.
-   - Merge or join relevant information from these datasets based on common keys.
 
-   ^^^ ABOVE WILL BE COMPLETED BY @JABALI in EDA process
+def logistic_regression_analysis():
+   # Step 1: Load Datasets
+   battles = pd.read_csv("data_files/battles.csv")
+   character_deaths = pd.read_csv("data_files/character-deaths.csv")
+   character_predictions = pd.read_csv("data_files/character-predictions.csv")
 
-3.  Feature Selection: 
-   - Identify relevant features for  our logistic regression model, such as age, nobility status, and gender.
-   - Convert categorical variables into numerical format using techniques like one-hot encoding.
+   # Step 2: Explore and Preprocess Data (Already Completed)
 
-4.  Define Target Variable: 
-   - Determine  our target variable, which in this case is whether a character died or survived.
+   # Step 3: Feature Selection
+   # Identify relevant features for our logistic regression model
+   selected_features = ['Gender', 'Nobility', 'GoT', 'CoK', 'SoS', 'FfC', 'DwD', 'Death Year', 'Book of Death', 'Death Chapter']
 
-5.  Split Data into Training and Testing Sets: 
-   - Use scikit-learn to split the data into training and testing sets. This helps evaluate  our model's performance.
+   # Step 4: Define Target Variable
+   target_variable = 'Death Year'  # Assuming 'Death Year' is our target variable
 
-6.  Build Logistic Regression Model: 
-   - Utilize the scikit-learn library to create a logistic regression model.
-   - Train the model using the training dataset.
+   # Step 5: Split Data into Training and Testing Sets
+   # Combine relevant features and target variable
+   data = character_deaths[selected_features + [target_variable]].copy()
 
-7.  Evaluate Model: 
-   - Assess the model's performance on the testing dataset using metrics like accuracy, precision, recall, and F1 score.
-   - Analyze the confusion matrix to understand the model's predictions.
+   # Handle missing values
+   data.fillna(0, inplace=True)  # Assuming missing values mean the absence of death
 
-8.  Interpret Coefficients: 
-   - Examine the coefficients of the logistic regression model to interpret the impact of each feature on the likelihood of death.
+   # Split data into features (X) and target variable (y)
+   X = data[selected_features]
+   y = data[target_variable]
 
-9.  Fine-tune Model: 
-   - Adjust model parameters or select different features based on the initial performance.
-   - Reevaluate the model's performance and iterate if necessary.
+   ''' 
+   # Split the data into training and testing sets
+   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-10.  Save Code in a Python Script: 
-    - Open a text editor or IDE, copy the Python code, and save it in a Python script file (e.g., `game_of_thrones_logistic_regression.py`).
+   # Step 6: Build Logistic Regression Model
+   # Use one-hot encoding for categorical variables
+   categorical_features = ['Gender', 'Nobility']
+   numeric_features = ['GoT', 'CoK', 'SoS', 'FfC', 'DwD', 'Death Year', 'Book of Death', 'Death Chapter']
 
-11.  Run Script and Save Output: 
-    - Execute the script using the Python interpreter in the command line or terminal.
-    - Optionally, save the script output to a file using the command-line redirection, for example:
-      ```bash
-      python game_of_thrones_logistic_regression.py > output.txt
-      ```
+   # Create a column transformer
+   preprocessor = ColumnTransformer(
+      transformers=[
+         ('num', 'passthrough', numeric_features),
+         ('cat', OneHotEncoder(drop='first'), categorical_features)  # Use drop='first' to avoid multicollinearity
+      ])
 
-"""
+   # Create a logistic regression model
+   model = Pipeline([
+      ('preprocessor', preprocessor),
+      ('classifier', LogisticRegression(random_state=42))
+   ])
+
+   # Step 7: Evaluate Model
+   # Train the model
+   model.fit(X_train, y_train)
+
+   # Make predictions on the test set
+   y_pred = model.predict(X_test)
+
+   # Evaluate the model
+   accuracy = accuracy_score(y_test, y_pred)
+   precision = precision_score(y_test, y_pred)
+   recall = recall_score(y_test, y_pred)
+   f1 = f1_score(y_test, y_pred)
+   conf_matrix = confusion_matrix(y_test, y_pred)
+
+   print(f"Accuracy: {accuracy:.2f}")
+   print(f"Precision: {precision:.2f}")
+   print(f"Recall: {recall:.2f}")
+   print(f"F1 Score: {f1:.2f}")
+   print("Confusion Matrix:")
+   print(conf_matrix)
+
+   # Step 8: Interpret Coefficients 
+   # Coefficients interpretation depends on the features selected and their transformations
+
+   # Step 9: Fine-tune Model
+   # Adjust model parameters or select different features based on the initial performance
+
+   # Step 10: Save Code in a Python Script
+
+   # Step 11: Run Script and Save Output 
+''' 
