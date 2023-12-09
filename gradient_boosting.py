ff6e29b32ddc954fbdfc7e11da3e64dfad71ab31
Franklin <franklin.guzman.dev@gmail.com>
2023-12-09 15:40:42 -0800

    more analysis

--- gradient_boosting.py
+++ gradient_boosting.py
@@ -1,13 +1,13 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import LabelEncoder
+
 def gradient_boosting_function():
-    character_predictions = pd.read_csv("data_files\character-predictions.csv")
-    character_deaths = pd.read_csv("data_files\character-deaths.csv")
+    character_predictions = pd.read_csv("data_files/character-predictions.csv")
+    character_deaths = pd.read_csv("data_files/character-deaths.csv")
 
     selected_features = ['isNoble', 'male', 'age', 'numDeadRelations', 'boolDeadRelations', 'isPopular', 'popularity']
 
@@ -20,19 +20,30 @@ def gradient_boosting_function():
     imputer = SimpleImputer(strategy='mean') 
     X_imputed = pd.DataFrame(imputer.fit_transform(X), columns=X.columns)
 
-
     X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.2, random_state=42)
 
     gb_regressor = GradientBoostingRegressor(learning_rate=0.1, n_estimators=1000, max_depth=3, random_state=42)
-
-
+    
     gb_regressor.fit(X_train, y_train)
 
     y_pred = gb_regressor.predict(X_test)
 
+
     mse = mean_squared_error(y_test, y_pred)
     print(f'Mean Squared Error: {mse}')
 
-    from sklearn.metrics import r2_score
+
     r2 = r2_score(y_test, y_pred)
     print(f'R^2 Score: {r2}')
+
+
+    mae = mean_absolute_error(y_test, y_pred)
+    print(f'Mean Absolute Error: {mae}')
+
+
+    feature_importance = gb_regressor.feature_importances_
+    feature_importance_dict = dict(zip(selected_features, feature_importance))
+    sorted_feature_importance = sorted(feature_importance_dict.items(), key=lambda x: x[1], reverse=True)
+    print('\nFeature Importance:')
+    for feature, importance in sorted_feature_importance:
+        print(f'{feature}: {importance}')
