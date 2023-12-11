601d0ddd212916af72cec8e8ff0bc3cf750b692f
Franklin <franklin.guzman.dev@gmail.com>
2023-12-10 17:47:24 -0800

    addded classification metrics

--- gradient_boosting.py
+++ gradient_boosting.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.ensemble import GradientBoostingClassifier  # Changed to classifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
@@ -16,34 +16,35 @@ def gradient_boosting_function():
     X = merged_data[selected_features]
     y = merged_data['actual']
 
+    # Convert to classication
+    y_binary = np.where(y > 0, 1, 0)
+
     # Imputing because some values are NaN/None
     imputer = SimpleImputer(strategy='mean') 
     X_imputed = pd.DataFrame(imputer.fit_transform(X), columns=X.columns)
 
-    X_train, X_test, y_train, y_test = train_test_split(X_imputed, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X_imputed, y_binary, test_size=0.2, random_state=42)
 
-    gb_regressor = GradientBoostingRegressor(learning_rate=0.1, n_estimators=1000, max_depth=3, random_state=42)
+    gb_classifier = GradientBoostingClassifier(learning_rate=0.1, n_estimators=1000, max_depth=3, random_state=42)
     
-    gb_regressor.fit(X_train, y_train)
+    gb_classifier.fit(X_train, y_train)
 
-    y_pred = gb_regressor.predict(X_test)
+    y_pred = gb_classifier.predict(X_test)
 
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    classification_rep = classification_report(y_test, y_pred)
 
-    mse = mean_squared_error(y_test, y_pred)
-    print(f'Mean Squared Error: {mse}')
+    # Print the classification evaluation metrics
+    print(f"Accuracy: {accuracy}")
+    print(f"Confusion Matrix:\n{conf_matrix}")
+    print(f"Classification Report:\n{classification_rep}")
 
-
-    r2 = r2_score(y_test, y_pred)
-    print(f'R^2 Score: {r2}')
-
-
-    mae = mean_absolute_error(y_test, y_pred)
-    print(f'Mean Absolute Error: {mae}')
-
-
-    feature_importance = gb_regressor.feature_importances_
+    feature_importance = gb_classifier.feature_importances_
     feature_importance_dict = dict(zip(selected_features, feature_importance))
     sorted_feature_importance = sorted(feature_importance_dict.items(), key=lambda x: x[1], reverse=True)
     print('\nFeature Importance:')
     for feature, importance in sorted_feature_importance:
         print(f'{feature}: {importance}')
+
+gradient_boosting_function()
